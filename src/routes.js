222326f93c61b0014565e69482652da1a6023a55
Walderlan Sena <senawalderlan@gmail.com>
2020-05-31 17:01:50 -0300

    feat: Add new packages and added Controller Session, Users.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello test' }));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+routes.get('/users', UserController.index);
+routes.get('/user/:id', UserController.show);
 
 export default routes;
